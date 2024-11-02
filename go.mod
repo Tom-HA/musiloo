78923d76cea2b919a754474b9b85d3525a671772
Tom <tom.harpaz98@gmail.com>
2024-11-02 23:35:46 +0200

    feat: wip spotify integration

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/tom-ha/musiloo
 
 go 1.23.2
 
-require github.com/eclipse/paho.mqtt.golang v1.5.0
+require (
+	github.com/eclipse/paho.mqtt.golang v1.5.0
+	github.com/zmb3/spotify/v2 v2.4.2
+	golang.org/x/oauth2 v0.23.0
+)
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -4,17 +4,37 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"github.com/zmb3/spotify/v2"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
 	"syscall"
 
+	"context"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"github.com/zmb3/spotify/v2/auth"
 )
 
 type MessagePayload struct {
 	MotionDetected bool `json:"motion_detected"`
+}
+
+type MQTTConfig struct {
+	server   string
+	topic    string
+	clientID string
+	username string
+	password string
+	qos      int
+}
+type SpotifyHandlerConfig struct {
+	writer http.ResponseWriter
+	req    *http.Request
+	auth   *spotifyauth.Authenticator
+	state  string
+	chn    chan<- *spotify.Client
 }
 
 func onMessageReceived(client MQTT.Client, message MQTT.Message) {
@@ -44,42 +64,113 @@ func main() {
 
 	hostname, _ := os.Hostname()
 
-	server := getEnv("MQTT_SERVER", "tcp://127.0.0.1:1883")
-	topic := getEnv("MQTT_TOPIC", "#")
-	clientID := getEnv("MQTT_CLIENT_ID", hostname)
-	username := getEnv("MQTT_USERNAME", "")
-	password := getEnv("MQTT_PASSWORD", "")
-	qos, err := strconv.Atoi(getEnv("MQTT_QOS", "0"))
-	if err != nil {
-		panic("Failed to parse MQTT QOS")
-	}
-	spotifyPlaylistName := getEnv("SPOTIFY_PLAYLIST_NAME", "")
-	if spotifyPlaylistName == "" {
-		panic("Please specify SPOTIFY_PLAYLIST_NAME")
+	mqttConfig := &MQTTConfig{
+		server:   getEnv("MQTT_SERVER", "tcp://127.0.0.1:1883"),
+		topic:    getEnv("MQTT_TOPIC", "#"),
+		clientID: getEnv("MQTT_CLIENT_ID", hostname),
+		username: getEnv("MQTT_USERNAME", ""),
+		password: getEnv("MQTT_PASSWORD", ""),
+		qos: func() int {
+			qos, err := strconv.Atoi(getEnv("MQTT_QOS", "0"))
+			if err != nil {
+				log.Fatal("Failed to parse MQTT QOS")
+			}
+			return qos
+		}(),
 	}
 
-	connOptions := MQTT.NewClientOptions().AddBroker(server).SetClientID(clientID).SetCleanSession(true)
-	if username != "" {
-		connOptions.SetUsername(username)
-		if password != "" {
-			connOptions.SetPassword(password)
+	spotifyPlaylistName := getEnv("SPOTIFY_PLAYLIST_NAME", "")
+	if spotifyPlaylistName == "" {
+		log.Fatal("Please specify SPOTIFY_PLAYLIST_NAME")
+	}
+
+	connOptions := getMQTTConnOptions(*mqttConfig)
+	authenticateSpotify()
+
+	mqttClient := MQTT.NewClient(connOptions)
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected successfully to %s\n", mqttConfig.server)
+	}
+
+	<-chn
+}
+
+func getMQTTConnOptions(config MQTTConfig) (mqttOptions *MQTT.ClientOptions) {
+	connOptions := MQTT.NewClientOptions().AddBroker(config.server).SetClientID(config.clientID).SetCleanSession(true)
+	if config.username != "" {
+		connOptions.SetUsername(config.username)
+		if config.password != "" {
+			connOptions.SetPassword(config.password)
 		}
 	}
 	tlsConfig := &tls.Config{InsecureSkipVerify: true, ClientAuth: tls.NoClientCert}
 	connOptions.SetTLSConfig(tlsConfig)
 
 	connOptions.OnConnect = func(c MQTT.Client) {
-		if token := c.Subscribe(topic, byte(qos), onMessageReceived); token.Wait() && token.Error() != nil {
-			panic(token.Error())
+		if token := c.Subscribe(config.topic, byte(config.qos), onMessageReceived); token.Wait() && token.Error() != nil {
+			log.Fatal(token.Error())
 		}
 	}
 
-	client := MQTT.NewClient(connOptions)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	} else {
-		fmt.Printf("Connected successfully to %s\n", server)
+	return connOptions
+}
+
+func authenticateSpotify() {
+	const redirectURI = "http://localhost:8080/callback"
+
+	var (
+		auth  = spotifyauth.New(spotifyauth.WithRedirectURL(redirectURI), spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate))
+		ch    = make(chan *spotify.Client)
+		state = "musiloo"
+	)
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		spotifyHandlerConfig := &SpotifyHandlerConfig{
+			writer: w,
+			req:    r,
+			auth:   auth,
+			state:  state,
+			chn:    ch,
+		}
+		spotifyClientHandler(*spotifyHandlerConfig)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received request at:", r.URL.String())
+	})
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+
+	client := <-ch
+
+	user, err := client.CurrentUser(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
+}
+
+func spotifyClientHandler(cfg SpotifyHandlerConfig) {
+	tok, err := cfg.auth.Token(cfg.req.Context(), cfg.state, cfg.req)
+	if err != nil {
+		http.Error(cfg.writer, "Couldn't get token", http.StatusForbidden)
+		log.Fatalln(err)
+	}
+	if st := cfg.req.FormValue("state"); st != cfg.state {
+		http.NotFound(cfg.writer, cfg.req)
+		log.Fatalf("State mismatch: %s != %s\n", st, cfg.state)
 	}
 
-	<-chn
+	spotifyClient := spotify.New(cfg.auth.Client(cfg.req.Context(), tok))
+	fmt.Printf("Login Completed!\n%s\n", cfg.writer)
+
+	cfg.chn <- spotifyClient
 }
